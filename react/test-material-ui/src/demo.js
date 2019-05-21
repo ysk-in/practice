8ecdf2dbb01f75bae198491237e3658122ae425d
ysk-in <7m7110123@gmail.com>
2019-05-21 15:19:29 +0900

    no message

--- react/test-material-ui/src/demo.js
+++ react/test-material-ui/src/demo.js
@@ -18,11 +18,28 @@ import Tooltip from "@material-ui/core/Tooltip";
 import DeleteIcon from "@material-ui/icons/Delete";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import { lighten } from "@material-ui/core/styles/colorManipulator";
+import ldevs from "./ldevs.js";
 
 let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
-  counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
+// function createData(name, calories, fat, carbs, protein) {
+//   counter += 1;
+//   return { id: counter, name, calories, fat, carbs, protein };
+// }
+
+function createLdevData() {
+  const ldev_data = [];
+  for (const ldev of JSON.parse(ldevs).data) {
+    counter += 1;
+    ldev_data.push({
+      id: counter,
+      ldevId: ldev.ldevId,
+      blockCapacity: ldev.blockCapacity,
+      byteFormatCapacity: ldev.byteFormatCapacity,
+      attributes: ldev.attributes.toString(),
+      status: ldev.status
+    });
+  }
+  return ldev_data;
 }
 
 function desc(a, b, orderBy) {
@@ -54,14 +71,29 @@ function getSorting(order, orderBy) {
 const rows = [
   {
     id: "name",
-    numeric: false,
+    numeric: true,
     disablePadding: true,
-    label: "Dessert (100g serving)"
+    label: "LDEV ID"
   },
-  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
-  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
-  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
-  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" }
+  {
+    id: "blockCapacity",
+    numeric: false,
+    disablePadding: false,
+    label: "BlockCapacity"
+  },
+  {
+    id: "byteFormatCapacity",
+    numeric: false,
+    disablePadding: false,
+    label: "Capacity"
+  },
+  {
+    id: "attributes",
+    numeric: false,
+    disablePadding: false,
+    label: "Attributes"
+  },
+  { id: "status", numeric: false, disablePadding: false, label: "Status" }
 ];
 
 class EnhancedTableHead extends React.Component {
@@ -92,7 +124,8 @@ class EnhancedTableHead extends React.Component {
             row => (
               <TableCell
                 key={row.id}
-                align={row.numeric ? "right" : "left"}
+                // align={row.numeric ? "right" : "left"}
+                align={"right"}
                 padding={row.disablePadding ? "none" : "default"}
                 sortDirection={orderBy === row.id ? order : false}
               >
@@ -169,7 +202,7 @@ let EnhancedTableToolbar = props => {
           </Typography>
         ) : (
           <Typography variant="h6" id="tableTitle">
-            Nutrition
+            LDEVS
           </Typography>
         )}
       </div>
@@ -216,25 +249,26 @@ const styles = theme => ({
 class EnhancedTable extends React.Component {
   state = {
     order: "asc",
-    orderBy: "calories",
+    orderBy: "ldevId",
     selected: [],
-    data: [
-      createData("Cupcake", 305, 3.7, 67, 4.3),
-      createData("Donut", 452, 25.0, 51, 4.9),
-      createData("Eclair", 262, 16.0, 24, 6.0),
-      createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-      createData("Gingerbread", 356, 16.0, 49, 3.9),
-      createData("Honeycomb", 408, 3.2, 87, 6.5),
-      createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-      createData("Jelly Bean", 375, 0.0, 94, 0.0),
-      createData("KitKat", 518, 26.0, 65, 7.0),
-      createData("Lollipop", 392, 0.2, 98, 0.0),
-      createData("Marshmallow", 318, 0, 81, 2.0),
-      createData("Nougat", 360, 19.0, 9, 37.0),
-      createData("Oreo", 437, 18.0, 63, 4.0)
-    ],
+    data: createLdevData(),
+    // data: [
+    //   createData("Cupcake", 305, 3.7, 67, 4.3),
+    //   createData("Donut", 452, 25.0, 51, 4.9),
+    //   createData("Eclair", 262, 16.0, 24, 6.0),
+    //   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    //   createData("Gingerbread", 356, 16.0, 49, 3.9),
+    //   createData("Honeycomb", 408, 3.2, 87, 6.5),
+    //   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    //   createData("Jelly Bean", 375, 0.0, 94, 0.0),
+    //   createData("KitKat", 518, 26.0, 65, 7.0),
+    //   createData("Lollipop", 392, 0.2, 98, 0.0),
+    //   createData("Marshmallow", 318, 0, 81, 2.0),
+    //   createData("Nougat", 360, 19.0, 9, 37.0),
+    //   createData("Oreo", 437, 18.0, 63, 4.0),
+    // ],
     page: 0,
-    rowsPerPage: 5
+    rowsPerPage: 10
   };
 
   handleRequestSort = (event, property) => {
@@ -324,13 +358,20 @@ class EnhancedTable extends React.Component {
                       <TableCell padding="checkbox">
                         <Checkbox checked={isSelected} />
                       </TableCell>
-                      <TableCell component="th" scope="row" padding="none">
-                        {n.name}
+                      <TableCell
+                        align="right"
+                        component="th"
+                        scope="row"
+                        padding="none"
+                      >
+                        {n.ldevId}
                       </TableCell>
-                      <TableCell align="right">{n.calories}</TableCell>
-                      <TableCell align="right">{n.fat}</TableCell>
-                      <TableCell align="right">{n.carbs}</TableCell>
-                      <TableCell align="right">{n.protein}</TableCell>
+                      <TableCell align="right">{n.blockCapacity}</TableCell>
+                      <TableCell align="right">
+                        {n.byteFormatCapacity}
+                      </TableCell>
+                      <TableCell align="right">{n.attributes}</TableCell>
+                      <TableCell align="right">{n.status}</TableCell>
                     </TableRow>
                   );
                 })}
