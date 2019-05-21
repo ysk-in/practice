8d8755f916fd96003fd2f373ccfad69d89c1f283
ysk-in <7m7110123@gmail.com>
2019-05-21 12:00:54 +0900

    Copy of Material-UI Table Sorting & Selecting (https://material-ui.com/demos/tables/#sorting-amp-selecting)

---
+++ react/test-material-ui/src/index.js
@@ -0,0 +1,5 @@
+import React from "react"; // eslint-disable-line no-unused-vars
+import ReactDOM from "react-dom";
+import Demo from "./demo";  // eslint-disable-line no-unused-vars
+
+ReactDOM.render(<Demo />, document.querySelector("#root"));
