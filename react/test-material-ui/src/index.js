05f67c26eb6856538ec7f6c903c810849290358f
ysk-in <7m7110123@gmail.com>
2019-05-21 16:30:55 +0900

    Use MUI-Datatables

--- react/test-material-ui/src/index.js
+++ react/test-material-ui/src/index.js
@@ -1,5 +1,56 @@
-import React from "react"; // eslint-disable-line no-unused-vars
+import React from "react";
 import ReactDOM from "react-dom";
-import Demo from "./demo";  // eslint-disable-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
+import MUIDataTable from "mui-datatables";
+import ldevs from "./ldevs";
 
-ReactDOM.render(<Demo />, document.querySelector("#root"));
+function readLdevs() {
+  const ldev_data = [];
+  for (const ldev of JSON.parse(ldevs).data) {
+    ldev_data.push([
+      ldev.ldevId,
+      ldev.blockCapacity,
+      ldev.byteFormatCapacity,
+      ldev.attributes.toString(),
+      ldev.status,
+      ldev.emulationType,
+      "raidLevel" in ldev ? ldev.raidLevel : "-",
+      "raidType" in ldev ? ldev.raidType : "-"
+    ]);
+  }
+  return ldev_data;
+}
+
+// eslint-disable-next-line no-unused-vars
+class App extends React.Component {
+  render() {
+    const columns = [
+      "LDEV ID",
+      "BlockCapacity",
+      "ByteFormatCapacity",
+      "Attributes",
+      "Status",
+      "EmulationType",
+      "RaidLevel",
+      "RaidType"
+    ];
+
+    const data = readLdevs();
+
+    const options = {
+      filterType: "multiselect",
+      responsive: "scroll"
+    };
+
+    return (
+      <MUIDataTable
+        title={"LDEVS"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
