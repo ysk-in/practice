19705a3ccc5e224103a3635c65e793a2026e23e8
ysk-in <7m7110123@gmail.com>
2019-06-18 13:39:50 +0900

    no message

--- react-redux/seasons/src/SeasonDisplay.js
+++ react-redux/seasons/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import "./SeasonDisplay.css";
 import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake"
+  }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -10,14 +22,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`${icon} icon`} />
-      <h1>
-        {season === "winter" ? "Burr, it is chilly" : "Lets hit the beach"}
-      </h1>
-      <i className={`${icon} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
