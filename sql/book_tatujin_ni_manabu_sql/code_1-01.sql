1bd3aeacb60b5e78dfcebb0a485afa2223e950f9
yskin <7m7110123@gmail.com>
2020-03-03 17:37:48 +0900

    Update tatujin_ni_manabu_sql ch.1-1

--- sql/book_tatujin_ni_manabu_sql/code_1-01.sql
+++ sql/book_tatujin_ni_manabu_sql/code_1-01.sql
@@ -5,21 +5,21 @@ USE tatujin;
 -- 日本語使うため utf8mb4 に設定しておきたい 予期しない動作防止するため
 -- SHOW VARIABLES like 'char%';
 
-CREATE TABLE PopTbl (
+CREATE TABLE pop (
     pref_name VARCHAR(32) PRIMARY KEY,
     population INTEGER NOT NULL
 );
 
-INSERT INTO PopTbl(pref_name, population) VALUES('徳島', 100), ('香川', 200) , ('愛媛', 150) , ('高知', 200) , ('福岡', 300) , ('佐賀', 100) , ('長崎', 200) , ('東京', 400) , ('群馬', 50);
+INSERT INTO pop(pref_name, population) VALUES('徳島', 100), ('香川', 200) , ('愛媛', 150) , ('高知', 200) , ('福岡', 300) , ('佐賀', 100) , ('長崎', 200) , ('東京', 400) , ('群馬', 50);
 
-CREATE TABLE PopTbl2 (
+CREATE TABLE pop2 (
     pref_name VARCHAR(32),
     sex CHAR(1) NOT NULL,
     population INTEGER NOT NULL,
     PRIMARY KEY (pref_name , sex)
 );
 
-INSERT INTO PopTbl2(pref_name, sex, population) VALUES('徳島', '1',	60 ), ('徳島', '2', 40 ), ('香川', '1', 100), ('香川', '2', 100), ('愛媛', '1', 100), ('愛媛', '2', 50 ), ('高知', '1', 100), ('高知', '2', 100), ('福岡', '1', 100), ('福岡', '2', 200), ('佐賀', '1', 20 ), ('佐賀', '2', 80 ), ('長崎', '1', 125), ('長崎', '2', 125), ('東京', '1', 250), ('東京', '2', 150);
+INSERT INTO pop2(pref_name, sex, population) VALUES('徳島', '1',	60 ), ('徳島', '2', 40 ), ('香川', '1', 100), ('香川', '2', 100), ('愛媛', '1', 100), ('愛媛', '2', 50 ), ('高知', '1', 100), ('高知', '2', 100), ('福岡', '1', 100), ('福岡', '2', 200), ('佐賀', '1', 20 ), ('佐賀', '2', 80 ), ('長崎', '1', 125), ('長崎', '2', 125), ('東京', '1', 250), ('東京', '2', 150);
 
 
 SELECT 
@@ -33,5 +33,69 @@ SELECT
         ELSE 0
     END) AS cnt_f
 FROM
-    PopTbl2
+    pop2
 GROUP BY pref_name;
+
+-- exercise 1-1
+CREATE TABLE greatests (
+    `key` char(1) primary key not null,
+    x int,
+    y int,
+    z int
+);
+INSERT INTO greatests VALUES("A", 1, 2, 3), ("B", 5, 5, 2), ("C", 4, 7, 1), ("D", 3, 3, 8);
+
+SELECT 
+    `key`,
+    CASE
+        WHEN x >= y THEN x
+        ELSE y
+    END AS greatest
+FROM
+    greatests;
+
+SELECT 
+    `key`,
+    CASE
+        WHEN x >= y && x >= z THEN x
+        WHEN y >= x && y >= z THEN y
+        ELSE z
+    END AS greatest
+FROM
+    greatests;
+
+-- exercise 1-2
+SELECT 
+    CASE
+        WHEN sex = 1 THEN '男'
+        WHEN sex = 2 THEN '女'
+        ELSE NULL
+    END AS '性別',
+    SUM(population) AS '全国',
+    SUM(CASE
+        WHEN pref_name = '徳島' THEN population
+    END) AS '徳島',
+        SUM(CASE
+        WHEN pref_name = '香川' THEN population
+    END) AS '香川',
+    -- 省略
+    SUM(CASE
+        WHEN pref_name IN ('徳島' , '香川', '高知', '愛媛') THEN population
+    END) AS '四国(再掲)'
+FROM
+    pop2
+GROUP BY sex;
+
+-- exercise 1-3
+SELECT 
+    `key`
+FROM
+    greatests
+ORDER BY CASE `key`
+    WHEN 'B' THEN 1
+    WHEN 'A' THEN 2
+    WHEN 'D' THEN 3
+    WHEN 'C' THEN 4
+    ELSE NULL
+END
+;
